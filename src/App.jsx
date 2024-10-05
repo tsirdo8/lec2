df3d0fc2cc7664280fe234fa32754f51eecd2b51
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2024-10-05 17:09:03 +0400

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import "./App.css";
 import Card from "./components/card/Card.jsx";
-import Header from "./components/header/Header.jsx";
+import { cards } from "./Data.js";
 
 function App() {
+  const handleClick = () => {
+    alert("Button clicked!");
+    console.log("Button clicked!");
+  };
+  const newColor = () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    document.querySelector(".Circle").style.backgroundColor = randomColor;
+    console.log("Background color changed to: ", randomColor);
+  };
   return (
     <div>
-      <Header/>
-      <Card
-        name={"nika"}
-        age={15}
-        description={
-          "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,illum"
-        }
-      />
-
-      <Card name={"saba"} age={16} bg={"green"} />
+      <img src="/assets/Dog.jpg" alt="dog" />
+      <div className="Circle" onMouseOver={newColor}></div>
+      <button onClick={handleClick}>Click me</button>
+      {cards.map((card) => (
+        <Card
+          key={card}
+          name={`${card.name}`}
+          age={card.age}
+          bg={"#" + Math.floor(Math.random() * 16777215).toString(16)}
+          description={card.description}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,6 @@
+export const  cards = [
+  { id: 1, name: "Gio", age: 15, description: "This is the first card" },
+  { id: 2, name: "Nika", age: 15, description: "This is the second card" },
+  { id: 3, name: "Saba", age: 16, description: "This is the third card" },
+  { id: 4, name: "Sandro", age: 14, description: "This is the fourth card" },
+];
